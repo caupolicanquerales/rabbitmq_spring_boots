c60f6e7e0c6700d28d3ac02c1fa60e70e793e3ad
caupolican <caupolicanquerales@gmail.com>
2021-10-01 17:57:18 -0300

    Nuevos cambios para regresar mensaje al arduino

---
+++ src/main/java/com/capo/rabbitmq/dto/BlinkNumber.java
@@ -0,0 +1,16 @@
+package com.capo.rabbitmq.dto;
+
+public class BlinkNumber {
+	
+	private int number;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+}
